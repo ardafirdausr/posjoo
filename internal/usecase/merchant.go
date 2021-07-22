1b8a0e0547a052341c67d7947b474a90d4a032c0
Arda <ardafirdausr@gmail.com>
2021-07-22 21:35:09 +0700

    add merchant usecase

---
+++ internal/usecase/merchant.go
@@ -0,0 +1,27 @@
+package usecase
+
+import (
+	"ardafirdausr/posjoo-server/internal"
+	"ardafirdausr/posjoo-server/internal/entity"
+	"log"
+)
+
+type MerchantUsecase struct {
+	merchantRepo internal.MerchantRepository
+}
+
+func NewMerchantUsecase(merchantRepo internal.MerchantRepository) *MerchantUsecase {
+	usecase := new(MerchantUsecase)
+	usecase.merchantRepo = merchantRepo
+	return usecase
+}
+
+func (uc *MerchantUsecase) CreateMerchant(param entity.CreateMerchantParam) (*entity.Merchant, error) {
+	merchant, err := uc.merchantRepo.CreateMerchant(param)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	return merchant, nil
+}
